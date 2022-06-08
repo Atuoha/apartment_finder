6814d41e8e8235f2a32cfeb16cffb8f11cbb1e45
Atuoha <atuohainitiatives@gmail.com>
2022-06-08 09:55:30 +0100

    initial setup

---
+++ android/app/src/main/kotlin/com/example/real_estates/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.real_estates
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/house.dart
@@ -0,0 +1,25 @@
+class House {
+  final String name;
+  final String location;
+  final List<String> imgUrls;
+  final int bedrooms;
+  final int bathrooms;
+  final int garage;
+  final int amount;
+  final String description;
+  final int kitchens;
+  final int sqm;
+
+  House({
+    required this.name,
+    required this.location,
+    required this.imgUrls,
+    required this.bedrooms,
+    required this.bathrooms,
+    required this.garage,
+    required this.amount,
+    required this.description,
+    required this.kitchens,
+    required this.sqm,
+  });
+}
