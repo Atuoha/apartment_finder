2afd3d3942bd18e1a489b9d40fad718d5cf57fc1
Atuoha <atuohainitiatives@gmail.com>
2022-06-09 22:20:01 +0100

    agent list overview adjustments

--- lib/components/single_agent_card.dart
+++ lib/components/single_agent_card.dart
@@ -30,57 +30,55 @@ class SingleAgentCard extends StatelessWidget {
       );
     }
 
-    return Column(
-      children: [
-        CircleAvatar(
+    return Stack(children: [
+      Positioned(
+        top: 53,
+        right: 0,
+        left: 0,
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          color: Colors.white,
+          elevation: 3,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                customWidget(Icons.person, name),
+                const SizedBox(height: 5),
+                customWidget(Icons.phone, mobile),
+                Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: kPurple,
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: Text(
+                        'View Listings',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      Container(
+        alignment: Alignment.topCenter,
+        child: CircleAvatar(
           radius: 35,
           backgroundColor: Colors.white,
           backgroundImage: NetworkImage(imgUrl),
         ),
-        Stack(
-          children: [
-            Container(
-              height: 90,
-              width: double.infinity,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                color: Colors.white,
-                elevation: 4,
-                child: Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      customWidget(Icons.person, name),
-                      const SizedBox(height: 5),
-                      customWidget(Icons.phone, mobile),
-                      Center(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5),
-                            color: kPurple,
-                          ),
-                          child: const Padding(
-                            padding: EdgeInsets.all(5.0),
-                            child: Text(
-                              'View Listings',
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
-      ],
-    );
+      )
+    ]);
   }
 }
